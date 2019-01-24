d164a08a064b167b76e916ad653f4b85661053d5
siam <rafshanulsiam811@gmail.com>
2019-01-25 02:43:40 +0600

    learning how to use router link navlink redirect prompt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect,
+  Prompt
+} from "react-router-dom";
 
+const User = params => {
+  return <h1>welcome user {params.username}</h1>;
+};
 class App extends Component {
+  state = {
+    loggedIn: false
+  };
+  loginHandle = () => {
+    this.setState(prevState => ({
+      loggedIn: !prevState.loggedIn
+    }));
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <NavLink to="/" exact activeStyle={{ color: "green" }}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" exact activeStyle={{ color: "green" }}>
+                about
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/user/siam" exact activeStyle={{ color: "green" }}>
+                about siam
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/user/shipa" exact activeStyle={{ color: "green" }}>
+                about shipa
+              </NavLink>
+            </li>
+          </ul>
+
+          <Prompt
+            when={!this.state.loggedIn}
+            message={location => {
+              return location.pathname.startsWith("/user")
+                ? "are you sure ?"
+                : true;
+            }}
+          />
+          <input
+            type="button"
+            value={this.state.loggedIn ? " log out" : "log in"}
+            onClick={this.loginHandle.bind(this)}
+          />
+          <Route
+            exact
+            strict
+            path="/"
+            render={props => {
+              return <h2>welcome router</h2>;
+            }}
+          />
+          <Route
+            exact
+            strict
+            path="/about"
+            render={props => {
+              return <h2>about router</h2>;
+            }}
+          />
+          <Route
+            exact
+            strict
+            path="/user/:username"
+            render={({ match }) =>
+              this.state.loggedIn ? (
+                <User username={match.params.username} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+        </div>
+      </Router>
     );
   }
 }
